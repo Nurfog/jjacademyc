96ee8a68b24c8d8a5488f4c753aec73952a41ce9
Nurfog <juan.allende@gmail.com>
2024-11-13 01:02:56 -0300

    adsf

--- lms/views.py
+++ lms/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def dashboard(request):
-    
+    user = User.objects.get(username=request.user.username, password=request.user.password)
     return render(request, 'pages/dashboard.html')
 
 
