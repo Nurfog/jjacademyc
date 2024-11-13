c7191f7f0dc6af9ec537d8990a6476bdb486d8ce
Nurfog <juan.allende@gmail.com>
2024-11-13 00:58:42 -0300

    actualizacion base datos LMS

--- lms/models.py
+++ lms/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class PlanEstudios(models.Model):
     idplan = models.AutoField(primary_key=True,null=False)
@@ -55,11 +58,40 @@ class Notas(models.Model):
         return self.nota
 
 
-class MisCursos(models.Model):
-    idmiscurso = models.AutoField(primary_key=True,null=False)
+class Profesores(models.Model):
+    idprofesor = models.AutoField(primary_key=True,null=False)
+    rutpasaporte = models.CharField(max_length=50,null=False)    
+    username = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    direccion = models.CharField(max_length=200,null=False)
+    comuna = models.CharField(max_length=200,null=False)
+    fono= models.CharField(max_length=50,null=False)    
+    activo = models.BooleanField(default=True,null=False)
+
+
+
+class Cursosabiertos(models.Model):
+    idcursoaberto = models.AutoField(primary_key=True,null=False)
     idplan = models.ForeignKey(PlanEstudios, on_delete=models.CASCADE)
     idcurso = models.ForeignKey(Cursos, on_delete=models.CASCADE)
-    
+    fechainicio = models.DateTimeField(null=True)
+    fechafin = models.DateTimeField(null=True)
+    admisioninicio = models.DateTimeField(null=True)
+    admisionfin = models.DateTimeField(null=True)
+    idasignacion = models.ForeignKey(Profesores, on_delete=models.CASCADE,null=True)
     activo = models.BooleanField(default=True,null=False)
+
+class AsignacionProfesores(models.Model):
+    idasignacion = models.AutoField(primary_key=True,null=False)
+    idprofesor = models.ForeignKey(Profesores, on_delete=models.CASCADE,null=True)
+    idcursoaberto = models.ForeignKey(Cursosabiertos, on_delete=models.CASCADE,null=True)
+    activo = models.BooleanField(default=True,null=False)
+
+
+class MisCursos(models.Model):
+    idmiscurso = models.AutoField(primary_key=True,null=False)
+    idcursoabierto = models.ForeignKey(Cursosabiertos, on_delete=models.CASCADE, null=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, null=True)    
+    activo = models.BooleanField(default=True,null=False)
+
 
 
