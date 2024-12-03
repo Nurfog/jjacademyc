1ac3c5eeef874dc09accddadc950692c48973463
Nurfog <juan.allende@gmail.com>
2024-12-03 18:02:45 -0300

    03122024

--- lms/views.py
+++ lms/views.py
@@ -3,21 +3,17 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from lms.models import *
 from django.db import connection
-from lms.classes import spMiscursos
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
 def dashboard(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        if username is not None:
-            results = spMiscursos.ListarMiscursos(request, username)
-            return render(request, 'pages/dashboard.html', {'results': results})
     return render(request, 'pages/dashboard.html')
 
-def compras(request):  
-    return render(request, 'pages/compras.html')
+def compras(request):
+    
+    return render(request, 'pages/compras.html',)
 
 def logout(request):
     return render(request, 'pages/login.html')
