e23532ff4aa6797479dcbda2c9da14ad39f36658
Nurfog <juan.allende@gmail.com>
2024-12-30 17:58:49 -0300

    inicio cms

---
+++ cms/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from cms import views
+from autenticacion.templates import *
+from autenticacion.views import *
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('cms/dashboard/', views.dashboard, name='cmsdashboard'),
+    path('cms/cursos/', views.compras, name='cmscursos'),
+    path('cms/planestudios/', views.planestudios, name='cmsplanestudios'),
+    path('cms/cursosabiertos/', views.cursosabiertos, name='cmscursosabiertos'),
+    path('cms/ponderaciones/', views.ponderaciones, name='cmsponderaciones'),
+    path('cms/profesores/', views.profesores, name='cmsprofesores'),
+    path('cms/tipoasignaciones/', views.tipoasignaciones, name='cmsasignaciones'),
+]
--- cms/views.py
+++ cms/views.py
@@ -2,3 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')
+
+
+def cursos(request):
+    return render(request, 'pages/cursos.html')
+
+
+def planestudios(request):
+    return render(request, 'pages/planestudios.html')
+
+def cursosabiertos(request):
+    return render(request, 'pages/cursosabiertos.html')
+
+def ponderaciones(request):
+    return render(request, 'pages/ponderaciones.html')
+
+def profesores(request):
+    return render(request, 'pages/profesores.html')
+
+def tipoasignaciones(request):
+    return render(request, 'pages/tipoasignaciones.html')
+
+def compras(request):
+    return render(request, 'pages/compras.html')
+
