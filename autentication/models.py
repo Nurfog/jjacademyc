f04adf9b044d2940cccd6329e7231a4e41884c1b
Nurfog <juan.allende@gmail.com>
2025-01-06 17:21:43 -0300

    1

---
+++ autentication/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    # Aquí puedes agregar campos adicionales si es necesario
+    pass
